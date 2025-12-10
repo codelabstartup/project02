82e510fdcb583536280f4f6fa6e4ee3469b5b42b
HoHyeon0124 <sdnsp0124@gmail.com>
2025-12-10 13:56:58 +0900

    게시판 목록, 상세, 댓글, 글쓰기 페이지 작업중

---
+++ src/Pages/BoardDetail.jsx
@@ -0,0 +1,17 @@
+// 상세 + 댓글 페이지
+
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function BoardDetail() {
+  const { id } = useParams();
+  const [postData, setPostData] = useState(null);
+  const [comment, setComment] = useState({ writer: "", content: "" });
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/board/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPostData(data));
+  }, [id]);
+
+}
--- src/Pages/BoardPage.jsx
+++ src/Pages/BoardPage.jsx
@@ -1,10 +1,49 @@
+// 게시판 목록 페이지
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "@emotion/styled"
 
-export default function BoardPage() {
-  return <Container>"게시판"</Container>
-}
+function BoardPage() {
+  const [posts, setPosts] = useState([]);
 
-const Container = styled.div`
-  width: 100%;
-  background-color: #f9f9f9;
-`
+  useEffect(() => {
+    fetch("http://localhost:8000/board?page=1&size=10")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+ return (
+    <div>
+      <h2></h2>
+      <Link to="/board/write">글쓰기</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>조회수</th>
+            <th>작성일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((p) => (
+            <tr key={p.ip_id}>
+              <td>{p.ip_id}</td>
+              <td>
+                <Link to={`/board/${p.ip_id}`}>{p.ip_title}</Link>
+              </td>
+              <td>{p.ip_writer}</td>
+              <td>{p.ip_view_count}</td>
+              <td>{p.ip_created_at}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default BoardList;
+
+
+
