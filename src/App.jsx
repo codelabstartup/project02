95b2256c64a384b7cf66ea268d1abde980b822fb
psallo <treasure0613@gmail.com>
2025-12-11 09:25:45 +0900

    app.jsx 파일 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,11 @@ import Containers from "./Containers"
 import "./App.css"
 import { ResultDataProvider } from "./context/ResultDataContext"
 
-import BoardList from "./Pages/BoardList"
-import BoardDetail from "./Pages/BoardDetail"
-import BoardWrite from "./Pages/BoardDetail"
-
 function App() {
   return (
     <BrowserRouter>
       <ResultDataProvider>
-        <Containers>
-      <Routes>
-          <Route path="/" element={<div>메인 페이지</div>} />
-          <Route path="/board" element={<BoardList />} />
-          <Route path="/board/write" element={<BoardWrite />} />
-          <Route path="/board/:id" element={<BoardDetail />} />
-        </Routes>
-        </Containers>
+        <Containers />
       </ResultDataProvider>
     </BrowserRouter>
   )
--- src/Pages/BoardPage.jsx
+++ src/Pages/BoardPage.jsx
@@ -1,49 +1,39 @@
-// 게시판 목록 페이지
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Box, Typography } from "@mui/material"
 import styled from "@emotion/styled"
 
-function BoardPage() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/board?page=1&size=10")
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, []);
-
- return (
-    <div>
-      <h2></h2>
-      <Link to="/board/write">글쓰기</Link>
-      <table>
-        <thead>
-          <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>조회수</th>
-            <th>작성일</th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((p) => (
-            <tr key={p.ip_id}>
-              <td>{p.ip_id}</td>
-              <td>
-                <Link to={`/board/${p.ip_id}`}>{p.ip_title}</Link>
-              </td>
-              <td>{p.ip_writer}</td>
-              <td>{p.ip_view_count}</td>
-              <td>{p.ip_created_at}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+export default function AboutPage() {
+  return "보드"
 }
-export default BoardList;
 
-
-
+const Container = styled.div`
+  background-color: #f9f9f9;
+`
+const Title = styled.div`
+  padding-top: 4em;
+  margin-bottom: 4em;
+  text-align: center;
+`
+const H5 = styled.h5`
+  font-size: 1em;
+  margin-bottom: 1em;
+`
+const H1 = styled.h1`
+  font-size: 3em;
+  margin-bottom: 0.4em;
+`
+const H6 = styled.h6`
+  font-size: 1em;
+`
+const BoxWrap = styled.div`
+  display: flex;
+  padding: 2em 2em;
+`
+const ContentBox = styled.div`
+  flex: 8;
+  background-color: #d2f8dc;
+  border-radius: 15px;
+  height: 300px;
+`
+const EmptyBox = styled.div`
+  flex: 2;
+`
